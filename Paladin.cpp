2a6cf5cfdc20a944254ed272fab3605a1f174e10
Tomasz Jaskula <thomasv1000@hotmail.fr>
2024-05-01 09:40:22 +0000

    Fix Paladin and DefensiveItem usage

--- Paladin.cpp
+++ Paladin.cpp
@@ -4,6 +4,8 @@
 Paladin::Paladin(std::string name_, int hp, int armor_) : Character(hp, armor_, 10),
 name(name_)
 {
+    helpfulItems = makeHelpfulItems(2);
+    defensiveItems = makeDefensiveItems(6);
 }
 
 const std::string& Paladin::getName()
