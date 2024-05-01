5ef167df60f3041e3f47434b73caa6ef1c8fcbcd
Tomasz Jaskula <thomasv1000@hotmail.fr>
2024-05-01 10:05:31 +0000

    Fix helpful and defensive items on Dwarf

--- Dwarf.cpp
+++ Dwarf.cpp
@@ -4,6 +4,8 @@
 Dwarf::Dwarf(std::string name_, int hp, int armor_) : Character(hp, armor_, 4),
 name(name_)
 {
+    helpfulItems = makeHelpfulItems(2);
+    defensiveItems = makeDefensiveItems(5);
 }
 
 const std::string& Dwarf::getName()
